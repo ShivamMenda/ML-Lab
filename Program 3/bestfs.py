f4e32f788216b18819a2e01d1ab2afedb9518752
Shivam Menda <shivammenda2002@gmail.com>
2023-07-08 00:04:10 +0530

    Naive bayes Updated

--- Program 3/bestfs.py
+++ Program 3/bestfs.py
@@ -1,66 +1,46 @@
-class Node:
-    def __init__(self,v,weight):
-        self.v=v
-        self.weight=weight
-class pathNode:
-    def __init__(self,node,parent):
-        self.node=node
-        self.parent=parent
-        
-def addEdge(u,v,weight):
-        adj[u].append(Node(v,weight))
-adj=[]
-def BFS(h,v,src,dest):
-    openlist=[]
-    closelist=[]
-    openlist.append(pathNode(src,None))
-    while(openlist):
-        currentNode=openlist[0]
-        currentIndex=0
-        for i in range(len(openlist)):
-            if (h[openlist[i].node]<h[currentNode.node]):
-                currentNode=openlist[i]
-                currentIndex=i
-        openlist.pop(currentIndex)
-        closelist.append(currentNode)
-        if (currentNode.node==dest):
-            path=[]
-            cur=currentNode
-            while(cur!=None):
-                path.append(cur.node)
-                cur=cur.parent
-            path.reverse()
-            return path
-        for node in adj[currentNode.node]:
-            for x in openlist:
-                if (x.node==node.v):
-                    continue
-            for x in closelist:
-                if (x.node==node.v):
-                    continue
-            openlist.append(pathNode(node.v,currentNode))
-    return openlist
+from queue import PriorityQueue
 
-v=10
-adj=[]
-for i in range(v):
-    adj.append([])
+def best_first_search(graph, start, goal, heuristic):
+    visited = set()
+    pq = PriorityQueue()
+    pq.put((heuristic[start], start))
     
-addEdge(0,1,2)
-addEdge(0,2,1)
-addEdge(0,3,10)
-addEdge(1,4,3)
-addEdge(1,5,2)
-addEdge(2,6,9)
-addEdge(3,7,5)
-addEdge(3,8,2)
-addEdge(7,9,5)
+    while not pq.empty():
+        _, node = pq.get()
+        
+        if node == goal:
+            print("Goal reached!")
+            return
+        
+        if node not in visited:
+            print("Visiting node:", node)
+            visited.add(node)
+            
+            for neighbor, _ in graph[node]:
+                if neighbor not in visited:
+                    pq.put((heuristic[neighbor], neighbor))
+    
+    print("Goal not found!")
 
-h=[20,22,21,10,25,24,30,5,12,0]
-path=BFS(h,v,0,9)
+# Example graph representation using adjacency list
+graph = {
+    'A': [('B', 4), ('C', 2)],
+    'B': [('A', 4), ('D', 5)],
+    'C': [('A', 2), ('E', 3)],
+    'D': [('B', 5), ('E', 1)],
+    'E': [('C', 3), ('D', 1)]
+}
 
-for i in range(len(path)-1):
-    print(path[i], end="->")
-print(path[len(path)-1])
+start_node = 'A'
+goal_node = 'E'
 
-#Output: 0->3->7->9
+#Heuristic values from curr node -> goal node
+heuristic_values = {
+    'A': 5,
+    'B': 2,
+    'C': 3,
+    'D': 4,
+    'E': 0
+}
+
+best_first_search(graph, start_node, goal_node, heuristic_values)
