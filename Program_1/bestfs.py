a7b0bf99a38139e7a45e92383e261c3437ba1c3d
Shivam Menda <shivammenda2002@gmail.com>
2023-07-08 20:42:00 +0530

    Prog 1 updated

--- Program_1/bestfs.py
+++ Program_1/bestfs.py
@@ -6,41 +6,51 @@ def best_first_search(graph, start, goal, heuristic):
     pq.put((heuristic[start], start))
     
     while not pq.empty():
-        _, node = pq.get()
+        h,node = pq.get()
         
         if node == goal:
-            print("Goal reached!")
+            print("Goal reached:", node)
             return
         
         if node not in visited:
-            print("Visiting node:", node)
-            visited.add(node)
-            
-            for neighbor, _ in graph[node]:
+            for neighbor in graph[node]:
                 if neighbor not in visited:
                     pq.put((heuristic[neighbor], neighbor))
+            
+            print("Visiting node:", node)
+            visited.add(node)
     
     print("Goal not found!")
 
 # Example graph representation using adjacency list
 graph = {
-    'A': [('B', 4), ('C', 2)],
-    'B': [('A', 4), ('D', 5)],
-    'C': [('A', 2), ('E', 3)],
-    'D': [('B', 5), ('E', 1)],
-    'E': [('C', 3), ('D', 1)]
+    'S':[('A'),('B')],
+    'A': [('C'), ('D')],
+    'B': [('E'), ('F')],
+    'C': [],
+    'D': [],
+    'E': [('H')],
+    'F': [('I'), ('G')],
+    'H':[],
+    'I':[],
+    'G':[],
 }
 
-start_node = 'A'
-goal_node = 'E'
+start_node = 'S'
+goal_node = 'G'
 
 #Heuristic values from curr node -> goal node
 heuristic_values = {
-    'A': 5,
-    'B': 2,
-    'C': 3,
-    'D': 4,
-    'E': 0
+    'S': 13,
+    'A': 12,
+    'B': 4,
+    'C': 7,
+    'D': 3,
+    'E': 8,
+    'F': 2,
+    'H': 4,
+    'I': 9,
+    'G': 0,
 }
 
 best_first_search(graph, start_node, goal_node, heuristic_values)
