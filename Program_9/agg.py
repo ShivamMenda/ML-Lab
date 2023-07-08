b52ae8729c67d4a5d6f8df18ea85d2e1cad4c093
Shivam Menda <shivammenda2002@gmail.com>
2023-07-08 19:50:29 +0530

    Prog 8 and 9 updated

--- Program_9/agg.py
+++ Program_9/agg.py
@@ -1,10 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.cluster.hierarchy import dendrogram, linkage
+import pandas as pd
+from scipy.cluster.hierarchy import dendrogram
 
-# Load the iris dataset
-iris = np.loadtxt('iris.csv', delimiter=',', skiprows=1)
-
+df=np.array([[4,4],
+                 [8,4],
+                 [15,8],
+                 [24,4],
+                 [24,12]])
 # Function to calculate the proximity matrix based on single-linkage
 def single_linkage(data):
     n = data.shape[0]
@@ -30,28 +33,26 @@ def complete_linkage(data):
     return proximity_matrix
 
 # Calculate the proximity matrix using single-linkage
-single_linkage_matrix = single_linkage(iris)
+single_linkage_matrix = single_linkage(df)
 print("Single-linkage proximity matrix:\n")
 print(single_linkage_matrix)
 
 # Calculate the proximity matrix using complete-linkage
-complete_linkage_matrix = complete_linkage(iris)
+complete_linkage_matrix = complete_linkage(df)
 print("\nComplete-linkage proximity matrix:\n")
 print(complete_linkage_matrix)
 
 # Plot the dendrogram using single-linkage
-linkage_matrix = linkage(iris, method='single')
 plt.figure(figsize=(10, 5))
-dendrogram(linkage_matrix)
+dendrogram(single_linkage_matrix)
 plt.title('Dendrogram - Single Linkage')
 plt.xlabel('Data Points')
 plt.ylabel('Distance')
 plt.show()
 
 # Plot the dendrogram using complete-linkage
-linkage_matrix = linkage(iris, method='complete')
 plt.figure(figsize=(10, 5))
-dendrogram(linkage_matrix)
+dendrogram(complete_linkage_matrix[:,:-1])
 plt.title('Dendrogram - Complete Linkage')
 plt.xlabel('Data Points')
 plt.ylabel('Distance')
